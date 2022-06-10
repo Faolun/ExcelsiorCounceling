e7c3ba9feb81636ea07279fb69d4e33ad7dee3b6
Faolun <james.coatesiii@gmail.com>
2022-06-10 16:49:59 -0400

    June2022 Update

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.parallax').parallax();
     $('.collapsible').collapsible();
+    $('.slider').slider({
+        full_width: true,
+        interval: 5000
+      });
+
+      $(".sAppt").click(function(){
+        alert("Simple Practice site coming soon!")
+    })
 });
+
